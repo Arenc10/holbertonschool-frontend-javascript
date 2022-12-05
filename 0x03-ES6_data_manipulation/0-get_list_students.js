35638f8b49f2e94baf3f8077679d5f106d88abee
Arenc <arenc.palluqi@yahoo.com>
2022-12-05 23:12:43 +0000

    Data structure exercises

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,20 @@
+const getListStudents = () => {
+	const myObj = [
+		{
+			id: 1,
+			firstName: 'Guillaume',
+			location: 'San Francisco'
+		},
+		{
+                        id: 2,
+                        firstName: 'James',
+                        location: 'Columbia'
+                },
+		{
+                        id: 5,
+                        firstName: 'Serena',
+                        location: 'San Francisco'
+                }
+	];
+	return myObj;
+}
